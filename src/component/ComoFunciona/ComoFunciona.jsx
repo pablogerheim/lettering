697d17fdc08915a046bba0f275dcf8802a9bc5d1
Gabriel Nader <gabrielgnader@gmail.com>
2022-11-02 10:00:53 -0300

    como funciona

--- src/component/ComoFunciona/ComoFunciona.jsx
+++ src/component/ComoFunciona/ComoFunciona.jsx
@@ -3,9 +3,9 @@ import "./ComoFunciona.css";
 function ComoFunciona() {
   return (
     <section className="comoFunciona">
-    <div className="comoFunciona-div">
-      <h3 className="comoFunciona-h3 align-left">Como funciona o curso</h3>
-      <p className="align-left ">
+    <div className="comoFunciona__content">
+      <h3>Como funciona o curso</h3>
+      <p>
         Esse é um treinamento passo a passo, onde você aprende lettering
         100% online. São mais de 90 aulas gravadas para você assistir no
         seu tempo, de onde quiser. Após a inscrição o acesso a plataforma
