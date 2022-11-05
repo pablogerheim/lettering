c60c3683112461b0cbff20b1aaa616b6dfdc1674
Gabriel Nader <gabrielgnader@gmail.com>
2022-11-05 10:15:45 -0300

    css carousel e alunos

--- src/component/CarouselCustom/CarouselCustom.jsx
+++ src/component/CarouselCustom/CarouselCustom.jsx
@@ -4,18 +4,20 @@ import Carousel from "react-elastic-carousel";
 function CarouselCustom(Props) {
   const breakPoints = [
     { width: 1, itemsToShow: 1 },
-    { width: 550, itemsToShow: 2, itemsToScroll: 2 },
+    { width: 550, itemsToShow: 2},
     { width: 850, itemsToShow: 3 },
     { width: 1150, itemsToShow: 4 },
-  ];
+  ]; 
   return (
-    <Carousel breakPoints={breakPoints}>
-      {Props.imagesList.map((item, index) => (
-        <div className={Props.className} key={index}>
-          <img alt={item.alt} src={item.img} />
-        </div>
-      ))}
-    </Carousel>
+    <div className="carousel__content">
+      <Carousel breakPoints={breakPoints} >
+        {Props.imagesList.map((item, index) => (
+          <div key={index}>
+            <img alt={item.alt} src={item.img} className="carousel__image"/>
+          </div>
+        ))}
+      </Carousel>
+    </div>
   );
 }
 
--- src/component/FacaArtes/FacaArtes.jsx
+++ src/component/FacaArtes/FacaArtes.jsx
@@ -1,14 +1,12 @@
 import "./FacaArtes.css";
-import { imgCamila } from "../../data/mock"
+import { imgCamila } from "../../data/mock";
 import CarouselCustom from "../CarouselCustom/CarouselCustom";
 
 function FacaArtes() {
   return (
     <section className="facaArtes">
       <h3>Faça artes como essas</h3>
-      <div className="box450">
-      <CarouselCustom className="imgAluno" imagesList={imgCamila} />
-      </div>
+      <CarouselCustom imagesList={imgCamila} />
     </section>
   );
 }
